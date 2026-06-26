a05d5da0268232712027c5a5d36f022ab93f9f91
Your Name <your.email@example.com>
2026-06-26 13:18:53 -0400

    he cambiado el archivo

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,7 +2,7 @@
 nombre = input("Por favor, ingresa tu nombre: ")
 
 # Mostrar las opciones de saludo
-print("\n¿Qué saludo prefieres?")
+
 print("1. Hola")
 print("2. Adiós")
 
@@ -11,10 +11,10 @@ while True:
     opcion = input("Elige una opción (1 o 2): ").strip()
     
     if opcion == "1":
-        print(f"\n¡Hola, {nombre}!")
+        print(f"\n¡Hola!")
         break
     elif opcion == "2":
-        print(f"\n¡Adiós, {nombre}!")
+        print(f"\n¡Adiós!")
         break
     else:
         print("Opción no válida. Por favor, escribe 1 o 2.")
